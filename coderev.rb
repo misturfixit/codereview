481c1fe46fd943a81af3131cfd292623b6d10652
misturfixit <japullins420@gmail.com>
2017-06-28 13:48:33 -0400

    whewhee

--- coderev.rb
+++ coderev.rb
@@ -1,6 +1,32 @@
 def this
 this1 = 1 + rand(9)
 end
+
+print "Guess a number between 1 & 9?"
+
+input = gets.chomp.to_i
+
+    if   input == rand
+        then 
+puts "Great job... You guessed the number."
+
+    elsif this  > input
+    	then
+puts "Sorry,... just a bit low..  :( "
+
+    elsif  this  < input
+    	then
+puts "Sorry,... Just a bit high ..  ;(  "
+   
+   # else
+
+#puts "huhwhat!?!?"
+end
+
+
 puts this
 
 
+
+
+ 
